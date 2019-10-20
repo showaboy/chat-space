943cb503ba7e7a2c6ee28916265965008a2011ed
showaboy <kenta.arai@hotmail.com>
2019-10-20 13:38:53 +0900

    gruop-function
    グループ作成機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+=======
+  devise_for :users
+  root to: "groups#index"
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
+>>>>>>> Stashed changes
 end
