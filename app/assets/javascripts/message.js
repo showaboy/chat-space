5f60567dab512d3272a92b4e9a9cd5a76e865fdb
showaboy <kenta.arai@hotmail.com>
2019-10-26 17:22:53 +0900

    remove       $('.messages').append(html)

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -34,7 +34,6 @@ $(document).on('turbolinks:load', function() {
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html)
-      $('#message_content').val('')
       $('.form__submit').prop('disabled',false)
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       $('#new_message')[0].reset();
